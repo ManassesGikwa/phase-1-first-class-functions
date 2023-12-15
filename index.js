22c2c4e2e2555fd7deb4d042609e4debce013bfe
ManassesGikwa <manassesgikwa@gmail.com>
2023-12-15 20:52:26 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function receivesAFunction(callback) {
+    // Call the callback function
+    callback();
+  }
+
+  function returnsANamedFunction()
+  {
+    return function NamedFunction(){}
+  }
+
+  function returnsAnAnonymousFunction()
+  {
+    return function(){}
+  }
